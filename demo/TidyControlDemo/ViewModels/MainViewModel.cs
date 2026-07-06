3cd245bab08fdd1a557fdfc3111eea9b396744e3
tangmanger <tangmanger@outlook.com>
2026-07-06 23:51:32 +0800

    init project

---
+++ demo/TidyControlDemo/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TidyControlDemo.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+
+        #region 命令
+
+        public RelayCommand LoadedCommand => new RelayCommand(() =>
+        {
+
+
+        });
+
+        #endregion
+    }
+}
---
+++ demo/TidyControlDemo/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TidyControlDemo.ViewModels
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            Ioc.Default.ConfigureServices(
+                new ServiceCollection()
+                    .AddSingleton<MainViewModel>()
+                    .BuildServiceProvider());
+        }
+        public MainViewModel Main => Ioc.Default.GetService<MainViewModel>();
+    }
+}
